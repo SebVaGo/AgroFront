bccfef4b340e734fb2a4876e9d79d99f94e5ae13
Sebastian <22sevago@gmail.com>
2024-10-31 03:07:46 -0500

    Arreglo del login

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -2,48 +2,42 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { userAccesToken } from '../../types';
+import fondoAgroWeb from '../../utils/images/FondoAgroWeb.webp';
+import logo from '../../utils/images/Logo.jpg';
 
 const Login = () => {
   const [formData, setFormData] = useState({
     correo: '',
     clave: '',
   });
-  const [message, setMessage] = useState('');  // Mensaje de éxito o error
-  const [accessToken, setAccessToken] = useState('');  // Token de autenticación
-  const [timeLeft, setTimeLeft] = useState<number>();  // Tiempo restante del token
-  const [perfiles, setPerfiles] = useState<Array<{
-    "id_usuario":number , "tipo_usuario":number
-  }>>();  // Perfiles disponibles si hay más de uno
-  const [selectedPerfil, setSelectedPerfil] = useState('');  // Perfil seleccionado por el usuario
-  //@ts-ignore
-  const [idUsuario, setIdUsuario] = useState<number>();  // ID del usuario
-  const navigate = useNavigate();  // Para redirigir a otras rutas
+  const [message, setMessage] = useState(''); 
+  const [accessToken, setAccessToken] = useState(''); 
+  const [timeLeft, setTimeLeft] = useState<number>(); 
+  const [perfiles, setPerfiles] = useState<Array<{ "id_usuario": number, "tipo_usuario": number }>>(); 
+  const [selectedPerfil, setSelectedPerfil] = useState('');
+  const [idUsuario, setIdUsuario] = useState<number>();  
+  const navigate = useNavigate(); 
 
-  // Maneja los cambios en los inputs
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
 
-  // Lógica para el login inicial
-  const handleLogin = async (e:React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await axios.post<userAccesToken>("https://agroweb-5dxm.onrender.com/api/auth/login", formData);
       const { accessToken, primerLogin, id_usuario, perfiles, seleccionRequerida } = response.data;
 
       if (seleccionRequerida && perfiles && perfiles.length > 0) {
-        // Si el usuario tiene múltiples perfiles, guardar perfiles y mostrar el selector
         setPerfiles(perfiles);
         setIdUsuario(id_usuario);
         setMessage('Seleccione un perfil para continuar.');
-        console.log(perfiles);
         return;
       }
 
-      // Si solo hay un perfil, continuar con el login
       sessionStorage.setItem('accessToken', accessToken);
       setAccessToken(accessToken);
       setMessage('¡Inicio de sesión exitoso!');
@@ -59,66 +53,57 @@ const Login = () => {
         setTimeLeft(timeResponse.data.timeLeft);
         startAutoLogoutTimer(timeResponse.data.timeLeft);
       }
-    } catch (error : unknown) {
+    } catch (error) {
       setMessage('Fallo en el inicio de sesión.');
-      const err = error as any;
-      console.error('Error durante el login:', err.response?.data?.error || err.message);
+      console.error('Error durante el login:', error);
     }
   };
 
-  // Lógica para enviar el perfil seleccionado
   const handlePerfilSeleccionado = async () => {
-    // Aquí buscamos el id_usuario correspondiente al tipo_usuario seleccionado
-    const perfilSeleccionado = perfiles?.find((perfil : any) => perfil.tipo_usuario === parseInt(selectedPerfil));
+    const perfilSeleccionado = perfiles?.find((perfil) => perfil.tipo_usuario === parseInt(selectedPerfil));
 
     if (!perfilSeleccionado) {
       setMessage('Perfil no válido seleccionado.');
       return;
     }
 
-    console.log('Perfil seleccionado:', selectedPerfil);
-    console.log('ID de usuario:', perfilSeleccionado);  // Este es el id_usuario correcto
-
     try {
       const response = await axios.post("https://agroweb-5dxm.onrender.com/api/auth/loginConPerfil", {
-        id_usuario: perfilSeleccionado.id_usuario,  // Aquí enviamos el id_usuario correcto
-        tipo_usuario: selectedPerfil,  // El perfil seleccionado por el usuario
+        id_usuario: perfilSeleccionado.id_usuario,
+        tipo_usuario: selectedPerfil,
       });
 
       const { accessToken } = response.data;
       sessionStorage.setItem('accessToken', accessToken);
       setAccessToken(accessToken);
       setMessage('¡Perfil seleccionado correctamente!');
-      navigate('/');  // Redirigir después de seleccionar el perfil
+      navigate('/');
     } catch (error) {
       setMessage('Error al seleccionar el perfil.');
       console.error('Error seleccionando perfil:', error);
     }
   };
 
-  // Lógica para auto-logout cuando el token expire
-  const startAutoLogoutTimer = (time : number) => {
+  const startAutoLogoutTimer = (time: number) => {
     const timer = setTimeout(() => {
       handleLogout();
-    }, time * 1000);  // El tiempo se recibe en segundos
+    }, time * 1000); 
 
-    return () => clearTimeout(timer);  // Limpiar el timeout si es necesario
+    return () => clearTimeout(timer); 
   };
 
-  // Función para cerrar sesión
   const handleLogout = () => {
     setAccessToken('');
     setTimeLeft(0);
     setMessage('Sesión expirada. Por favor, inicie sesión de nuevo.');
-    sessionStorage.removeItem('accessToken');  // Remover el token del sessionStorage
+    sessionStorage.removeItem('accessToken'); 
   };
 
-  // Manejar el tiempo restante en el token
   useEffect(() => {
     if (timeLeft && timeLeft > 0) {
       const timer = setTimeout(() => {
         setTimeLeft(timeLeft - 1);
-      }, 1000);  // Disminuir el tiempo restante cada segundo
+      }, 1000);
 
       return () => clearTimeout(timer);
     } else if (timeLeft === 0) {
@@ -127,81 +112,99 @@ const Login = () => {
   }, [timeLeft]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
-      <h2 className="text-2xl font-bold text-center text-gray-900">Iniciar Sesión</h2>
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${fondoAgroWeb})` }}>
+      <div className="flex w-full max-w-6xl bg-white rounded-3xl shadow-2xl overflow-hidden transform transition duration-500 hover:scale-105">
+        <div className="flex flex-col items-center justify-center w-1/2 p-12 bg-white-100">
+            <h1 className="text-5xl font-bold text-green-600 mt-0 mb-6">AgroWeb</h1>
+            <img
+                src={logo} 
+                alt="Imagen de AgroWeb"
+                className="rounded-2xl shadow-lg w-90 h-70 object-cover"
+            />
+            <p className="mt-4 text-center text-gray-700 text-lg">
+                Conéctate con el futuro de la agricultura sostenible.
+                Únete a nuestra comunidad y descubre oportunidades para crecer.
+            </p>
+        </div>
 
-      {!accessToken && (perfiles?.length ?? 0) === 0 ? (
-      <form onSubmit={handleLogin} className="space-y-6">
-      <div>
-        <label htmlFor="correo" className="block text-sm font-medium text-gray-700">Correo:</label>
-        <input
-        id="correo"
-        type="email"
-        name="correo"
-        value={formData.correo}
-        onChange={handleChange}
-        required
-        className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div>
-        <label htmlFor="clave" className="block text-sm font-medium text-gray-700">Clave:</label>
-        <input
-        id="clave"
-        type="password"
-        name="clave"
-        value={formData.clave}
-        onChange={handleChange}
-        required
-        className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <p className='text-center'>
-        ¿No tienes una cuenta? <a href="/register" className="text-sm text-blue-500 hover:underline"> Regístrate aquí.</a>
-      </p>
-      <button
-        type="submit"
-        className="w-full px-4 py-2 text-sm font-medium text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-      >
-        Login
-      </button>
-      </form>
-      ) : (perfiles?.length ?? 0) > 0 ? (
-      <div className="space-y-6">
-      <h3 className="text-lg font-medium text-gray-900">Seleccione su perfil:</h3>
-      <select
-        value={selectedPerfil}
-        onChange={(e) => setSelectedPerfil(e.target.value)}
-        className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      >
-        <option value="">Seleccione un perfil</option>
-        {(perfiles ?? []).map((perfil, index) => (
-        <option key={index} value={perfil.tipo_usuario}>
-        {perfil.tipo_usuario === 1 ? 'Comprador' : 'Vendedor'}
-        </option>
-        ))}
-      </select>
-      <button
-        onClick={handlePerfilSeleccionado}
-        className="w-full px-4 py-2 text-sm font-medium text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-      >
-        Continuar
-      </button>
-      </div>
-      ) : (
-      <div className="space-y-6">
-      <h3 className="text-lg font-medium text-gray-900">Tiempo restante del token: {timeLeft !== null ? `${timeLeft} segundos` : 'Calculando...'}</h3>
-      <button
-        onClick={handleLogout}
-        className="w-full px-4 py-2 text-sm font-medium text-white bg-green-500 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-      >
-        Logout
-      </button>
-      </div>
-      )}
+        {/* Columna Derecha: Formulario de Inicio de Sesión */}
+        <div className="flex items-center justify-center w-1/2 p-12">
+          <div className="w-full max-w-md space-y-8">
+            <h2 className="text-3xl font-semibold text-center text-gray-800">Iniciar Sesión</h2>
 
-      {message && <p className="mt-4 text-sm text-center text-red-600">{message}</p>}
+            {!accessToken && (perfiles?.length ?? 0) === 0 ? (
+              <form onSubmit={handleLogin} className="space-y-6">
+                <div>
+                  <label htmlFor="correo" className="block text-sm font-medium text-gray-700">Correo:</label>
+                  <input
+                    id="correo"
+                    type="email"
+                    name="correo"
+                    value={formData.correo}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-3 mt-1 border rounded-lg shadow-md focus:ring-green-500 focus:border-green-500"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="clave" className="block text-sm font-medium text-gray-700">Clave:</label>
+                  <input
+                    id="clave"
+                    type="password"
+                    name="clave"
+                    value={formData.clave}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-3 mt-1 border rounded-lg shadow-md focus:ring-green-500 focus:border-green-500"
+                  />
+                </div>
+                <p className="text-center text-gray-600">
+                  ¿No tienes una cuenta? <a href="/register" className="text-blue-600 hover:underline"> Regístrate aquí.</a>
+                </p>
+                <button
+                  type="submit"
+                  className="w-full px-6 py-3 text-lg font-semibold text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                >
+                  Login
+                </button>
+              </form>
+            ) : (perfiles?.length ?? 0) > 0 ? (
+              <div className="space-y-6">
+                <h3 className="text-lg font-medium text-gray-900">Seleccione su perfil:</h3>
+                <select
+                  value={selectedPerfil}
+                  onChange={(e) => setSelectedPerfil(e.target.value)}
+                  className="w-full px-4 py-3 mt-1 border rounded-lg shadow-md focus:ring-green-500 focus:border-green-500"
+                >
+                  <option value="">Seleccione un perfil</option>
+                  {(perfiles ?? []).map((perfil, index) => (
+                    <option key={index} value={perfil.tipo_usuario}>
+                      {perfil.tipo_usuario === 1 ? 'Comprador' : 'Vendedor'}
+                    </option>
+                  ))}
+                </select>
+                <button
+                  onClick={handlePerfilSeleccionado}
+                  className="w-full px-6 py-3 text-lg font-semibold text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                >
+                  Continuar
+                </button>
+              </div>
+            ) : (
+              <div className="space-y-6">
+                <h3 className="text-lg font-medium text-gray-900">Tiempo restante del token: {timeLeft !== null ? `${timeLeft} segundos` : 'Calculando...'}</h3>
+                <button
+                  onClick={handleLogout}
+                  className="w-full px-6 py-3 text-lg font-semibold text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+
+            {message && <p className="mt-4 text-sm text-center text-red-600">{message}</p>}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Register/RegisterPage.tsx
+++ src/Pages/Register/RegisterPage.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { datosUsuario, userDniRucData, UserType, userVerifyData } from "../../types";
 import { useNavigate } from "react-router-dom";
+import fondoAgroWeb from '../../utils/images/FondoAgroWeb.webp';
+import logo from '../../utils/images/Logo.jpg';
 
 export default function Register(){
     const nav = useNavigate();
@@ -130,70 +132,84 @@ export default function Register(){
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="max-w-lg mx-auto my-auto w-3/4 bg-white shadow-lg rounded-lg overflow-hidden"> 
-                <div className="px-8 py-6"> {/* Cambiado px-6 py-4 a px-8 py-6 */}
-                    <h2 className="text-2xl font-bold text-center text-gray-800">Registro de Usuario</h2>
-                    {inicio && <form onSubmit={handleSubmitDniRuc} className="mt-6">
-                        {/* Input para DNI o RUC */}
-                        <div className="mb-4">
-                            <label htmlFor="dniRuc" className="block text-gray-700">Ingrese su DNI o RUC:</label>
-                            <input
-                            id="dniRuc"
-                            type="text"
-                            name="dniRuc"
-                            value={formData.dniRuc}
-                            onChange={handleInputChange}
-                            required
-                            placeholder="Ingrese su DNI o RUC"
-                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                            />
-                        </div>
-
-                        {/* Selección de tipo de usuario */}
-                        <div className="mb-4">
-                            <label htmlFor="id_tipo_usuario" className="block text-gray-700">Tipo de Usuario:</label>
-                            <select
-                            id="id_tipo_usuario"
-                            name="id_tipo_usuario"
-                            value={formData.id_tipo_usuario}
-                            onChange={handleSelectChange}
-                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                            >
-                            {tiposUsuario && Array.isArray(tiposUsuario) && tiposUsuario.map(tipo => (
-                            <option key={tipo.id_tipo_usuario} value={tipo.id_tipo_usuario}>
-                            {tipo.tipo}
-                            </option>
-                            ))}
-                            </select>
-                        </div>
-
-                        {/* Botón de envío de DNI o RUC */}
-                        <div className="flex justify-center">
-                            <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
-                            Consultar
-                            </button>
-                        </div>
-                    </form>}
+        <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${fondoAgroWeb})` }}>
+            <div className="flex w-full max-w-6xl bg-white rounded-3xl shadow-xl overflow-hidden transform transition duration-500 hover:scale-105">
+                {/* Columna Izquierda: Información General */}
+                <div className="flex flex-col items-center justify-center w-1/2 p-12 bg-white-100">
+                    <h1 className="text-5xl font-bold text-green-600 mt-0 mb-6">AgroWeb</h1>
+                    <img
+                        src={logo} 
+                        alt="Imagen de AgroWeb"
+                        className="rounded-2xl shadow-lg w-90 h-70 object-cover"
+                    />
+                    <p className="mt-4 text-center text-gray-700 text-lg">
+                        Conéctate con el futuro de la agricultura sostenible.
+                        Únete a nuestra comunidad y descubre oportunidades para crecer.
+                    </p>
                 </div>
-                {datosUsuario && (
-                    <div className="p-4 ">
-                        <h3 className="text-xl font-semibold text-gray-800 mb-4">Datos Obtenidos</h3>
-                        {formData.dniRuc.length === 8 ? (
-                            <>
-                                <p className="text-gray-700 mb-2"><strong>Nombres:</strong> {`${datosUsuario.primer_nombre} ${datosUsuario.segundo_nombre || ''}`}</p>
-                                <p className="text-gray-700 mb-2"><strong>Apellidos:</strong> {`${datosUsuario.apellido_paterno} ${datosUsuario.apellido_materno}`}</p>
-                            </>
-                        ) : (
-                            <>
-                                <p className="text-gray-700 mb-2"><strong>Razón Social:</strong> {datosUsuario.razon_social}</p>
-                                <p className="text-gray-700 mb-2"><strong>Dirección:</strong> {`${datosUsuario.direccion}, ${datosUsuario.departamento}, ${datosUsuario.provincia}, ${datosUsuario.distrito}`}</p>
-                                <p className="text-gray-700 mb-2"><strong>Estado:</strong> {datosUsuario.estado}</p>
-                                <p className="text-gray-700 mb-2"><strong>Condición:</strong> {datosUsuario.condicion}</p>
-                            </>
-                        )}
-                    </div>
-                )}
+
+                {/* Columna Derecha: Formulario de Registro */}
+                <div className="w-1/2 p-8">
+                    <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Registro de Usuario</h2>
+                    
+                    {inicio && (
+                        <form onSubmit={handleSubmitDniRuc} className="space-y-6">
+                            <div>
+                                <label htmlFor="dniRuc" className="block text-gray-700 font-medium">Ingrese su DNI o RUC:</label>
+                                <input
+                                    id="dniRuc"
+                                    type="text"
+                                    name="dniRuc"
+                                    value={formData.dniRuc}
+                                    onChange={handleInputChange}
+                                    required
+                                    placeholder="Ingrese su DNI o RUC"
+                                    className="w-full px-4 py-3 mt-1 border rounded-lg shadow-sm focus:ring-2 focus:ring-green-500"
+                                />
+                            </div>
+                            
+                            <div>
+                                <label htmlFor="id_tipo_usuario" className="block text-gray-700 font-medium">Tipo de Usuario:</label>
+                                <select
+                                    id="id_tipo_usuario"
+                                    name="id_tipo_usuario"
+                                    value={formData.id_tipo_usuario}
+                                    onChange={handleSelectChange}
+                                    className="w-full px-4 py-3 mt-1 border rounded-lg shadow-sm focus:ring-2 focus:ring-green-500"
+                                >
+                                    {tiposUsuario && Array.isArray(tiposUsuario) && tiposUsuario.map(tipo => (
+                                        <option key={tipo.id_tipo_usuario} value={tipo.id_tipo_usuario}>
+                                            {tipo.tipo}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            
+                            <button type="submit" className="w-full bg-green-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+                                Consultar
+                            </button>
+                        </form>
+                    )}
+
+                    {/* Información de Usuario Obtenida */}
+                    {datosUsuario && (
+                        <div className="mt-6 space-y-4 text-gray-700">
+                            <h3 className="text-lg font-semibold">Datos Obtenidos</h3>
+                            {formData.dniRuc.length === 8 ? (
+                                <>
+                                    <p><strong>Nombres:</strong> {`${datosUsuario.primer_nombre} ${datosUsuario.segundo_nombre || ''}`}</p>
+                                    <p><strong>Apellidos:</strong> {`${datosUsuario.apellido_paterno} ${datosUsuario.apellido_materno}`}</p>
+                                </>
+                            ) : (
+                                <>
+                                    <p><strong>Razón Social:</strong> {datosUsuario.razon_social}</p>
+                                    <p><strong>Dirección:</strong> {`${datosUsuario.direccion}, ${datosUsuario.departamento}, ${datosUsuario.provincia}, ${datosUsuario.distrito}`}</p>
+                                    <p><strong>Estado:</strong> {datosUsuario.estado}</p>
+                                    <p><strong>Condición:</strong> {datosUsuario.condicion}</p>
+                                </>
+                            )}
+                        </div>
+                    )}
                 {datosUsuario && (
                     <div className=" p-4 pb-0 ">
                         <div className="mb-4">
@@ -311,6 +327,8 @@ export default function Register(){
                 
             </div>
         </div>
+    </div>
+
 
     )
 }
